649e0b6f91a11d86abc630e724e236a4a5c18ec6
mostafadesham <118707842+mostafadesham@users.noreply.github.com>
2022-11-21 06:23:13 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * GR8 Faucet Script Lite
+ * https://gr8.cc
+ * Edit By 7azemTN
+ * https://7azemtn.blogspot.com
+ */
+
+## Database settings
+$host = 'localhost';
+$database = '';
+$username = '';
+$password = '';
+
+## Faucet id, this should be changed if you have more than one installation.
+$faucetID = 'f1';
+
+## Show Error Messages - Set to false once installion is finished.
+$show_errors = true;
